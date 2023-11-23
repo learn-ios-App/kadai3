eed79eed27409ef2a8537758173295a51293ce12
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-23 18:57:26 +0900

    commit

--- kadai3/ContentView.swift
+++ kadai3/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var number1 = ""
+    @State private var number2 = ""
+    @State private var sum = 0
+    
+    @State private var isAlert = false
+    @State private var message = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack {
+                TextField("", text: $number1)
+                TextField("", text: $number2)
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .frame(width: 200)
+            .padding()
+            
+            Button(action: {
+                sum = calclation(number1, number2)
+            }) {
+                Text("計算")
+            }
+            Text("合計 \(sum)")
+                .padding()
         }
-        .padding()
+        .alert(message, isPresented: $isAlert, actions: {})
+    }
+    
+    private func calclation(_ number1: String, _ number2: String) -> Int {
+        if self.number1 == "" || self.number2 == "" {
+            showAlert(message: "数字を入力してください")
+        }
+        return parseNumber(number1) + parseNumber(number2)
+    }
+    private func showAlert(message: String) {
+        self.message = message
+        isAlert = true
+    }
+    private func parseNumber(_ number: String) -> Int {
+        return Int(number) ?? 0
     }
 }
 
