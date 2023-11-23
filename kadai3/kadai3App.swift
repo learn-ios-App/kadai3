0ef34df51a4881e797c38e65527c5dc4a4c00a00
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-11-23 18:34:33 +0900

    Initial Commit

---
+++ kadai3/kadai3App.swift
@@ -0,0 +1,17 @@
+//
+//  kadai3App.swift
+//  kadai3
+//
+//  Created by 渡邊魁優 on 2023/11/23.
+//
+
+import SwiftUI
+
+@main
+struct kadai3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
